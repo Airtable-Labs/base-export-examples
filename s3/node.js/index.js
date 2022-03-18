3724f1ddc796d539b83e76ee0516a25340363787
victoriaplummer <victoria.plummer@airtable.com>
2022-03-18 11:54:38 -0400

    Adding azure example

---
+++ s3/node.js/index.js
@@ -0,0 +1,16 @@
+// Declare Variables from .env
+
+// Declare Amazon S3 Variables
+// Load Base and Base Schema
+// Check if Each Folder Directory Exists, if not make one
+// For each table in a base....
+// Set Variables for Table Data
+// Get Table Data and create CSV from JSON
+// Get Array Fields
+// Format Linked Records
+// Format Table Data
+// Create CSV
+// Check s3 bucket for contents and instance of CSV
+// # If there are contents in the bucket check to see if file exists
+// # If File exists, download file, append, and upload to bucket then remove from staging
+// # If no file exists with that table id/name in s3 bucket uplpad the CSV Upload File
